4396b56d5d5f506da614755db93f9e90d18cabd5
matite <matiastite1@gmail.com>
2024-01-25 08:34:53 -0500

    Added scaffold theme to our main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text("Hola Mundo")));
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Center(
+          child: Text('Hello World!'),
+        )));
   }
 }
