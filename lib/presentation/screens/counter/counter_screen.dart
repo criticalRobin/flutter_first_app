31751be4e8295d5d5974c12d692d1ccdabf0cef1
matite <matiastite1@gmail.com>
2024-01-25 08:44:27 -0500

    Reordering screens in different folders

---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text('Counter Screen'),
+    ));
+  }
+}
